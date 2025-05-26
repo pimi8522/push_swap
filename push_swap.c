a87c7aa41c29d4f6525fa1473ef0f9fbf7459063
Miguel Duarte <miduarte@c2r6s12.42lisboa.com>
2025-05-26 11:14:22 +0100

    grah

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 14:39:13 by miduarte          #+#    #+#             */
-/*   Updated: 2025/05/23 15:39:37 by miduarte         ###   ########.fr       */
+/*   Updated: 2025/05/23 15:45:50 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int main(int argc, char **argv)
     t_node *stack_a = NULL; // A pointer to the top of stack A. Initially empty.
     t_node *stack_b = NULL; // A pointer to the top of stack B. Initially empty.
 
-    
-
+	
     return (0);
 }
