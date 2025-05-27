91665a7297de6d3d5af6a331f3fded7affc2baf4
Miguel Duarte <miduarte@c2r5s13.42lisboa.com>
2025-05-27 11:18:46 +0100

    prelims2

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 14:39:23 by miduarte          #+#    #+#             */
-/*   Updated: 2025/05/23 15:38:10 by miduarte         ###   ########.fr       */
+/*   Updated: 2025/05/27 11:01:52 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,34 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <stdbool.h>
+
 # include "./libft/libft.h"
 
+/*
+ * A node in the stack
+ */
 typedef struct s_node
 {
-	int				value;  // The actual integer value
-	int				index;  // The node's final, sorted position (more on this later)
-	struct s_node	*prev;  // Pointer to the previous node in the stack
-	struct s_node	*next;  // Pointer to the next node in the stack
-}	t_node;
+    int             value;
+	int				index;
+	int				cost;
+	bool			above_median;
+	bool			is_cheapest;
+    struct s_node  *prev;
+    struct s_node  *next;
+	struct s_node  *target;
+}               t_node;
+
+/*
+ * A stack with quick access to top, bottom, and its size
+ */
+typedef struct s_stack
+{
+    t_node  *top;
+    t_node  *bot;
+    int      size;
+}               t_stack;
+
 
 #endif
--- struct_utils.c
+++ struct_utils.c
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   struct_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/23 14:39:13 by miduarte          #+#    #+#             */
-/*   Updated: 2025/05/27 11:15:09 by miduarte         ###   ########.fr       */
+/*   Created: 2025/05/26 14:36:41 by miduarte          #+#    #+#             */
+/*   Updated: 2025/05/26 14:36:42 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
-
-int main(int argc, char **argv)
-{
-    t_node *stack_a; // A pointer to the top of stack A. Initially empty to avoid undefined behaviour.
-    t_node *stack_b; // A pointer to the top of stack B. Initially empty.
-	
-	
-	stack_a = NULL;
-	stack_b = NULL;
-	if (argc >= 2 && argv[1])
-		push_swap(argv[1]);
-	else
-		return (1);
-
-    return (0);
-}
